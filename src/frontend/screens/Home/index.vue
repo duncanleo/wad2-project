ed467b436866000f2ce3af0a91923844064c5753
Duncan Leo <hello@duncanleo.me>
2021-09-12 18:21:23 +0800

    feat: placeholder home page styling

--- src/frontend/screens/Home/index.vue
+++ src/frontend/screens/Home/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <p>{{ greeting }} World!</p>
+  <div class="container pt-5">
+    <div class="row align-items-center mb-5">
+      <div class="col-12 col-lg-5 mb-5 mb-lg-0 me-auto">
+        <h2 class="display-4 fw-bold mb-5">Explore the world.</h2>
+        <p class="lead text-muted mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+          amet ligula lacus. Vestibulum tristique, nisl consectetur commodo
+          ultricies, arcu nunc rutrum sapien, non consequat enim velit at lacus.
+        </p>
+        <div class="d-flex flex-wrap">
+          <router-link class="btn btn-primary me-2 mb-2 mb-sm-0" to="/signup">
+            Sign-up
+          </router-link>
+          <router-link class="btn btn-outline-dark mb-2 mb-sm-0" to="/login">
+            Login
+          </router-link>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <img class="img-fluid" src="https://via.placeholder.com/500" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
