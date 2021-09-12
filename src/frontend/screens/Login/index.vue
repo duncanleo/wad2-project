4a76b7f1e3f1e22a09a1fcf36e861ca19bde15ca
Duncan Leo <hello@duncanleo.me>
2021-09-12 18:05:22 +0800

    feat: semi-working login screen

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,14 +1,61 @@
 <template>
-  <form method="POST" action="/login">
-    <input type="text" placeholder="Email" />
-    <input type="password" placeholder="Password" />
-    <input type="submit" value="Login" />
-  </form>
+  <div class="mb-3">
+    <span class="error text-danger">{{ error }}</span>
+    <form method="POST" action="/login" v-on:submit="handleSubmit">
+      <input
+        class="form-control"
+        name="email"
+        type="text"
+        placeholder="Email"
+        v-model="email"
+      />
+      <input
+        class="form-control"
+        name="password"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
+      <input class="form-control" type="submit" value="Login" />
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-const Login = Vue.extend({});
+import axios from 'axios';
+
+const Login = Vue.extend({
+  data: function () {
+    return {
+      email: '',
+      password: '',
+      error: '',
+    };
+  },
+  methods: {
+    handleSubmit: async function (e: SubmitEvent) {
+      const { email, password } = this;
+
+      e.preventDefault();
+
+      try {
+        const response = await axios.post('/login', {
+          email,
+          password,
+        });
+
+        this.$router.push('/');
+      } catch (e) {
+        if ('response' in e) {
+          this.error = e.response.data.message;
+        } else {
+          this.error = `${e}`;
+        }
+      }
+    },
+  },
+});
 
 export default Login;
 </script>
