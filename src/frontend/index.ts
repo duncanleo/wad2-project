66456ecbfb47d8e3bbd43d84349e6f2d82a2aab9
Duncan Leo <hello@duncanleo.me>
2021-09-15 18:40:15 +0800

    chore(frontend): implement router-level auth gate

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -3,6 +3,7 @@ import 'bootstrap';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import axios from 'axios';
 import Vue from 'vue';
 import VueRouter, { RouterOptions } from 'vue-router';
 
@@ -16,6 +17,11 @@ import VueApp from './VueApp.vue';
 
 Vue.use(VueRouter);
 
+/**
+ * Route protection
+ * https://webomnizz.com/protecting-route-for-authenticated-users-in-vue-js/
+ **/
+
 const routes: RouterOptions['routes'] = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
@@ -24,16 +30,39 @@ const routes: RouterOptions['routes'] = [
   {
     path: '/feed',
     component: Feed,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/checkin',
     component: CheckIn,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
 const router = new VueRouter({
   routes,
   mode: 'history',
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (!requiresAuth) {
+    next();
+    return;
+  }
+
+  try {
+    // TODO: this will cause auth HTTP requests all the time
+    await axios.get('/auth');
+    next();
+  } catch (e) {
+    next('login');
+  }
 });
 
 /**
