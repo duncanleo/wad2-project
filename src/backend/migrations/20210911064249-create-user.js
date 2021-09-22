ffd0d237d7d4ccbb719881a20d890ad993663b1d
Duncan Leo <hello@duncanleo.me>
2021-09-22 15:54:59 +0800

    fix: incorrect typing

--- src/backend/migrations/20210911064249-create-user.js
+++ src/backend/migrations/20210911064249-create-user.js
@@ -2,7 +2,7 @@
 module.exports = {
   /**
    * @param {import('sequelize').QueryInterface} queryInterface
-   * @param {import('sequelize').Sequelize} Sequelize
+   * @param {import('sequelize')} Sequelize
    **/
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('users', {
@@ -35,7 +35,7 @@ module.exports = {
   },
   /**
    * @param {import('sequelize').QueryInterface} queryInterface
-   * @param {import('sequelize').Sequelize} Sequelize
+   * @param {import('sequelize')} Sequelize
    **/
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('users');
