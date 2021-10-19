620f130fe7f0e321a3981cd56fc43c55c32c93bd
Brant Neo <brantneozc@gmail.com>
2021-10-20 00:24:51 +0800

    Dashboard Page Styling
    Added:
    Upcoming tournament div block
    Placeholders are put in place for tournament details and tournament photos
    (Will replace all with the information in the database once ready)

--- src/frontend/screens/Home/screens/Dashboard/index.vue
+++ src/frontend/screens/Home/screens/Dashboard/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-white">Welcome, {{ user.display_name }}</h1>
     <input
       type="text"
-      style="width:100%; height:50px; background-color:#729B98; "
+      style="width: 100%; height: 50px; background-color: #729b98"
       class="border border-1 form-control"
       name=""
       v-model="searchTerm"
@@ -17,9 +17,17 @@
         v-bind:to="playerLink(player.id)"
         v-for="player in players"
         v-bind:key="player.id"
-        class= "fs-5 text-white"
+        class="fs-5 text-white"
       >
-        <span>{{ player.display_name }}</span>
+        <div
+          style="background-color: #729b98; height: 100px"
+          class="col-2 text-center"
+        >
+          <img src="" alt="" />
+          <span class="mx-auto d-block">
+            {{ player.display_name }}
+          </span>
+        </div>
       </router-link>
     </div>
     <div class="row" v-if="teams.length > 0">
@@ -46,37 +54,34 @@
     >
       <h4 class="text-white">No results</h4>
     </div>
-    <br>
 
-    <div 
-    style="background-color: #729B98;"
-    class="p-4 row">
+    <br />
+
+    <div style="background-color: #729b98" class="p-4 row">
       <h1 class="text-white">UPCOMING</h1>
-      <hr>
-
+      <hr />
 
       <div class="row justify-content-center">
         <!-- row -->
-        <div class="col-4 card m-4 text-white" style="width: 18rem; background-color:black;" v-for="n in 6">
+        <div
+          class="col-4 card m-4 text-white"
+          style="width: 18rem; background-color: black"
+          v-for="n in 6"
+        >
           <!-- col should be based on tournamentArr.length-->
-            <img class="card-img-top" src="" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">{{tournaments}}</h5>
-              <p class="card-text">tournamentStartDate</p>
-              <p class="card-text">tournamentEndDate</p>
-              <p class="card-text">tournamentPricePool</p>
-              <p class="card-text">tournamentOrganizer</p>
-              <a href="#" class="btn btn-outline-success">More Details</a>
-              <!-- # = tournament.link -->
-            </div>
-            <!-- col -->
+          <img class="card-img-top" src="" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">{{ tournaments }}</h5>
+            <p class="card-text">tournamentStartDate</p>
+            <p class="card-text">tournamentEndDate</p>
+            <p class="card-text">tournamentPricePool</p>
+            <p class="card-text">tournamentOrganizer</p>
+            <a href="#" class="btn btn-outline-success">More Details</a>
+            <!-- # = tournament.link -->
+          </div>
+          <!-- col -->
         </div>
-        <!-- row -->
       </div>
-
-
-
-
     </div>
   </div>
 </template>
@@ -86,15 +91,11 @@ import axios from 'axios';
 import { debounce } from 'lodash';
 import Vue from 'vue';
 
-
 interface SearchResponse extends App.API.ResponseBase {
   players: App.API.User[];
   teams: App.API.Team[];
   tournaments: App.API.Tournament[];
 }
-
-// var url = '../../../../../../backend/initdata/tournaments.json';
-
 
 const Dashboard = Vue.extend({
   data() {
@@ -103,20 +104,40 @@ const Dashboard = Vue.extend({
       players: [] as App.API.User[],
       teams: [] as App.API.Team[],
       tournaments: [] as App.API.Tournament[],
+      tournamentData: [],
     };
   },
 
-  // created: function(){
-  //   axios.get(url)
-  //     .then(response=> {
-  //       console.log(response.data)
-  //       this.tournaments = response.data;
-  //   })
-  //     .catch(error=>{
-  //       console.log(error.message)
-  //   })
-  // },
   computed: {
+    tournamentFilter() {
+      var upcomingTournament = [];
+      // var today = new Date()
+      var tournamentData = this.tournamentData;
+      for (let tournament of tournamentData) {
+        let startDate = tournament.start_at;
+        let endDate = tournament.end_at;
+        let date = startDate.split('T')[0];
+        let edate = endDate.split('T')[0];
+        tournament.start_at = date;
+        tournament.end_at = edate;
+        let dateValues = date.split('-');
+
+        if (Number(dateValues[0]) >= 2021) {
+          if (Number(dateValues[1]) >= 10) {
+            if (Number(dateValues[2]) >= 15) {
+              upcomingTournament.push(tournament);
+            }
+          } else {
+            continue;
+          }
+        } else {
+          continue;
+        }
+      }
+
+      return upcomingTournament;
+    },
+
     user() {
       const state = this.$store.state as App.Frontend.Store.RootState;
 
@@ -124,7 +145,16 @@ const Dashboard = Vue.extend({
     },
   },
 
+  beforeMount() {
+    this.apiTournament();
+  },
+
   methods: {
+    async apiTournament() {
+      const response = await axios.get('/api/tournaments', {});
+
+      this.tournamentData = response.data.tournaments;
+    },
     async search() {
       const response = await axios.post<SearchResponse>('/api/search', {
         term: this.searchTerm,
