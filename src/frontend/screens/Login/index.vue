edbbe16a347418808c924f1553616f1c392577c4
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-10-31 00:36:27 +0800

    Update index.vue

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-    <div class="col" style="background-color: #96B8B5; position: absolute; width: 700px; height: 550px; left: 363px; top: 150px;">
+  <div class="container-lg">
+    <div style="background-color: #96B8B5; position: absolute; width: 700px; height: 550px; left: 363px; top: 150px;">
       <form style="position: absolute; margin-top: 250px; margin-left: 45px" method="POST" action="/login" v-on:submit="handleSubmit">
         <h5>Login</h5>
         <input
