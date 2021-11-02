c9d07f8155e8b29bd343e61d8973419f749be4cd
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-11-03 03:28:43 +0800

    Update index.vue

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="row justify-content-center">
   <form class="form w-50 h-75" method="POST" action="/login" v-on:submit="handleSubmit">
-      <h5>Login</h5>
-      <input class="form-control ms-5" name="email" type="text" placeholder="Email Address" v-model="email"/>
+      <h5 class="ms-3">Login</h5>
+      <input class="form-control ms-3" name="email" type="text" placeholder="Email Address" v-model="email"/>
 
-      <input class="form-control ms-5" name="password" type="password" placeholder="Password" v-model="password"/>
+      <input class="form-control ms-3" name="password" type="password" placeholder="Password" v-model="password"/>
 
-      <input class="form-control ms-5" type="submit" value="Login"/>
+      <input class="form-control ms-3" type="submit" value="Login"/>
 
       <span class="error text-danger">{{ error }}</span>
     </form>
