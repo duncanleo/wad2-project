5b9b4057e32f9f51dfb1d19f4162969b30dc2f15
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-11-03 20:24:32 +0800

    Update index.vue

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="row justify-content-center" style="background-color: #96B8B5;">
-  <div class="col-8">
+<div class="row justify-content-center">
+  <div class="col-8 h-75" style="background-color: #96B8B5;">
     <form class="form" method="POST" action="/login" v-on:submit="handleSubmit">
       <h5 class="ms-1">Login</h5>
       <input class="form-control mb-1 ms-1 me-1" style="background: #729B98;" name="email" type="text" placeholder="Email Address" v-model="email"/>
