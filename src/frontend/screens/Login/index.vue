f590e797d84e27665a96093b039f34054314e5d7
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-11-03 16:34:07 +0800

    Update index.vue

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="row justify-content-center h-75">
+<div class="row justify-content-center">
   <form class="form w-50" method="POST" action="/login" v-on:submit="handleSubmit">
       <h5 class="ms-1">Login</h5>
       <input class="form-control ms-1 me-1" name="email" type="text" placeholder="Email Address" v-model="email"/>
