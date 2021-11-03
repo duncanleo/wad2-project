deef02c0b56915b279755ed518ad97712a0bdf76
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-11-03 18:10:46 +0800

    Update index.vue

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="d-block justify-content-center h-75">
+<div class="row justify-content-center">
     <form class="form w-50" method="POST" action="/login" v-on:submit="handleSubmit">
     
       <h5 class="ms-1">Login</h5>
