8cd8b54ffe455bf4c0c94c547cf9c1c9133ed029
Glenn Ham <glenn-ham@hotmail.com>
2021-11-03 18:07:09 +0800

    login page
    No icon and removed remember me box

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -2,27 +2,28 @@
 
 <template>
 
-  <div class="d-block justify-content-center w-100 h-50">
+  <div class="d-block justify-content-center w-100 h-50 bg-danger p-3">
     
 
 
     <!--login box-->
-    <div id = "loginDetails" class="mb-3 col-6 mx-auto p-5 rounded" style="background-color: #96B8B5;" >
+    <div id = "loginDetails" class="mb-3 col-12  mx-auto p-5 rounded" style="background-color: #96B8B5;" >
         <span class="error text-danger">{{ error }}</span>
 
     <!--Game logo-->
-        <div id = "gameLogo" class="text-danger col-6 mx-auto mb-5" style="border-style:solid; text-align:centre">
-          <img style="height:200px; display:block" v-bind:src="gamelogo" alt = "waiting">
-        </div>
-
-        <div style="display:flex">
-   
-        <div class="col-6 mx-auto mb-2">
-          <span class="h5"><b>Login</b></span></div>
+        <div id = "gameLogo" class="text-danger col-8 mx-auto mb-5" style="border-style:solid; text-align:centre">
+          <img style="height:100%; width:100%" v-bind:src="gamelogo" alt = "waiting">
         </div>
 
         
-        <form class = "col-6 mx-auto"  method="POST" action="/login" v-on:submit="handleSubmit" >
+   
+        <div class="mb-2">
+          <span class="h5"><b>Login</b></span>
+        </div>
+        
+
+        
+        <form class = "col-12 mx-auto"  method="POST" action="/login" v-on:submit="handleSubmit" >
         
         
         
@@ -44,8 +45,8 @@
             v-model="password"
           />
 
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-          <label for="vehicle1"><b>Remember Me</b></label><br>
+          <!-- <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"> -->
+          <!-- <label for="vehicle1"><b>Remember Me</b></label><br> -->
 
           <input id ="loginButton" style="background-color:#729B98; color:white; border-color:#729B98" class="form-control mt-4 mx-auto mb-3" type="submit" value="Login" />
           <a class="text-danger" href="../Signup">Dont have an account? Sign up here</a>
