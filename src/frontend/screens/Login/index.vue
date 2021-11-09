c63f7e22863fda735ab6c3ad04d0ead275c22205
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2021-11-09 18:18:44 +0800

    feat: Login & Signup pages (PR: #6)
    * created "About.html"
    
    * Delete "About.html"
    
    * Delete "About.html"
    
    * Create styles.scss
    
    * added class in index
    
    * added width to div class
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.html
    
    * Update index.html
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Update index.html
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.html
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update styles.scss
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.html
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Create logo.png
    
    * Update index.vue
    
    * Update index.vue
    
    * Update styles.scss
    
    * Update index.vue
    
    * Create styles.scss

--- src/frontend/screens/Login/index.vue
+++ src/frontend/screens/Login/index.vue
@@ -1,29 +1,26 @@
 <template>
-  <div class="mb-3">
-    <span class="error text-danger">{{ error }}</span>
-    <form method="POST" action="/login" v-on:submit="handleSubmit">
-      <input
-        class="form-control"
-        name="email"
-        type="text"
-        placeholder="Email"
-        v-model="email"
-      />
-      <input
-        class="form-control"
-        name="password"
-        type="password"
-        placeholder="Password"
-        v-model="password"
-      />
-      <input class="form-control" type="submit" value="Login" />
-    </form>
+<div class="row justify-content-center">
+  <div class="col-6 box" style="background-color: #96B8B5;">
+    <div style="margin-top: 270px">
+      <form method="POST" action="/login" v-on:submit="handleSubmit">
+      <h6 class="center mb-2" style="font-weight: bold">Login</h6>
+      <input class="form-control mb-2 center" style="background: #729B98;" name="email" type="text" placeholder="Email Address" v-model="email"/>
+
+      <input class="form-control mb-2 center" style="background: #729B98;" name="password" type="password" placeholder="Password" v-model="password"/>
+
+      <input class="form-control mb-2 w-25 center" style="background: #729B98; color: white" type="submit" value="Login"/>
+
+      <span class="error text-danger">{{ error }}</span>
+      </form>
+    </div>
   </div>
+</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import axios from 'axios';
+import './styles.scss';
 
 const Login = Vue.extend({
   data: function () {
