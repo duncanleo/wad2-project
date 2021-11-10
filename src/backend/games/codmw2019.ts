399028414b227e8fac2d4ee67ec242cb7d862010
Duncan Leo <hello@duncanleo.me>
2021-11-10 09:30:03 +0800

    chore(backend): switch CODMW API

--- src/backend/games/codmw2019.ts
+++ src/backend/games/codmw2019.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 type Platform = 'psn' | 'steam' | 'xbl' | 'battle' | 'uno' | 'acti';
 
-const API_URL = 'https://call-of-duty4.p.rapidapi.com/warzone';
+const API_URL = 'https://call-of-duty-modern-warfare.p.rapidapi.com/warzone';
 
 const { CODMW_2019_API_KEY } = process.env;
 
@@ -12,14 +12,16 @@ interface Response {
 }
 
 export default async function codmw2019(platform: Platform, gamertag: string) {
-  console.log('wow', platform, gamertag);
-  const response = await axios.get<Response>(API_URL, {
-    params: { platform, gamertag },
-    headers: {
-      'x-rapidapi-host': 'call-of-duty4.p.rapidapi.com',
-      'x-rapidapi-key': CODMW_2019_API_KEY,
-    },
-  });
+  const response = await axios.get<Response>(
+    `${API_URL}/${gamertag}/${platform}`,
+    {
+      params: { platform, gamertag },
+      headers: {
+        'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com',
+        'x-rapidapi-key': CODMW_2019_API_KEY,
+      },
+    }
+  );
 
-  return response.data.data;
+  return response.data;
 }
