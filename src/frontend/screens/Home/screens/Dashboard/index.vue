d7e28be16bdf23a06d2606c40bf6c475a96486cc
Brant Neo <brantneozc@gmail.com>
2021-11-12 02:34:18 +0800

    Styled Tournaments & Dashboard
    Added icons to both screens, styled the results for teams and tournaments

--- src/frontend/screens/Home/screens/Dashboard/index.vue
+++ src/frontend/screens/Home/screens/Dashboard/index.vue
@@ -17,18 +17,29 @@
         v-bind:to="playerLink(player.id)"
         v-for="player in players"
         v-bind:key="player.id"
-        class="fs-5 text-white"
+        style="text-decoration: none"
+        class="fs-5 text-white col-2 mb-4"
       >
         <div
           style="background-color: #729b98; height: 100px"
-          class="col-2 text-center"
+          class="text-center my-auto rounded"
         >
-          <img src="" alt="" />
+          <br />
           <span class="mx-auto d-block">
-            <img
-              class="rounded-circle"
-              src="src/frontend/assets/images/test.png"
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-person-circle mb-1"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path
+                fill-rule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+              />
+            </svg>
             {{ player.display_name }}
           </span>
         </div>
@@ -40,14 +51,61 @@
         v-bind:to="teamLink(team.id)"
         v-for="team in teams"
         v-bind:key="team.id"
+        style="text-decoration: none"
+        class="fs-5 text-white col-2 mb-4"
       >
-        <span>{{ team.name }}</span>
+        <div
+          style="background-color: #729b98; height: 100px"
+          class="text-center my-auto rounded"
+        >
+          <br />
+          <span class="mx-auto d-block">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-joystick mb-1 me-1"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2z"
+              />
+              <path
+                d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23z"
+              /></svg
+            >{{ team.name }}</span
+          >
+        </div>
       </router-link>
     </div>
     <div class="row" v-if="tournaments.length > 0">
       <h1 class="text-white">Tournaments</h1>
-      <div v-for="tournament in tournaments" v-bind:key="tournament.id">
-        <span>{{ tournament.name }}</span>
+      <div
+        v-for="tournament in tournaments"
+        v-bind:key="tournament.id"
+        style="text-decoration: none"
+        class="fs-5 text-white col-3 mb-4"
+      >
+        <div
+          style="background-color: #729b98; height: 150px"
+          class="text-center my-auto rounded pt-3"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-dpad-fill mb-2"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6.5 0A1.5 1.5 0 0 0 5 1.5v3a.5.5 0 0 1-.5.5h-3A1.5 1.5 0 0 0 0 6.5v3A1.5 1.5 0 0 0 1.5 11h3a.5.5 0 0 1 .5.5v3A1.5 1.5 0 0 0 6.5 16h3a1.5 1.5 0 0 0 1.5-1.5v-3a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 0 16 9.5v-3A1.5 1.5 0 0 0 14.5 5h-3a.5.5 0 0 1-.5-.5v-3A1.5 1.5 0 0 0 9.5 0h-3Zm1.288 2.34-.799 1.278A.25.25 0 0 0 7.201 4H8.8a.25.25 0 0 0 .212-.382l-.799-1.279a.25.25 0 0 0-.424 0Zm0 11.32-.799-1.277A.25.25 0 0 1 7.201 12H8.8a.25.25 0 0 1 .212.383l-.799 1.278a.25.25 0 0 1-.424 0Zm-4.17-4.65-1.279-.798a.25.25 0 0 1 0-.424l1.279-.799A.25.25 0 0 1 4 7.201V8.8a.25.25 0 0 1-.382.212Zm10.043-.798-1.278.799A.25.25 0 0 1 12 8.799V7.2a.25.25 0 0 1 .383-.212l1.278.799a.25.25 0 0 1 0 .424Z"
+            />
+          </svg>
+          <span class="">{{ tournament.name }}</span>
+        </div>
       </div>
     </div>
     <div
@@ -56,12 +114,39 @@
         players.length === 0 && teams.length === 0 && tournaments.length === 0
       "
     >
-      <h4 class="text-white">No results</h4>
+      <h4 class="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          fill="currentColor"
+          class="bi bi-search mb-1 me-2"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+          /></svg
+        >No results
+      </h4>
     </div>
     <br />
 
     <div style="background-color: #729b98" class="p-4 row">
-      <h1 class="text-white">UPCOMING</h1>
+      <h1 class="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          class="bi bi-calendar-event-fill mb-2 me-2"
+          style="color: #31625f"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"
+          /></svg
+        >UPCOMING
+      </h1>
       <hr />
 
       <div class="row justify-content-center">
@@ -72,6 +157,19 @@
         >
           <div class="card-body">
             <h4>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-dpad-fill mb-2"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6.5 0A1.5 1.5 0 0 0 5 1.5v3a.5.5 0 0 1-.5.5h-3A1.5 1.5 0 0 0 0 6.5v3A1.5 1.5 0 0 0 1.5 11h3a.5.5 0 0 1 .5.5v3A1.5 1.5 0 0 0 6.5 16h3a1.5 1.5 0 0 0 1.5-1.5v-3a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 0 16 9.5v-3A1.5 1.5 0 0 0 14.5 5h-3a.5.5 0 0 1-.5-.5v-3A1.5 1.5 0 0 0 9.5 0h-3Zm1.288 2.34-.799 1.278A.25.25 0 0 0 7.201 4H8.8a.25.25 0 0 0 .212-.382l-.799-1.279a.25.25 0 0 0-.424 0Zm0 11.32-.799-1.277A.25.25 0 0 1 7.201 12H8.8a.25.25 0 0 1 .212.383l-.799 1.278a.25.25 0 0 1-.424 0Zm-4.17-4.65-1.279-.798a.25.25 0 0 1 0-.424l1.279-.799A.25.25 0 0 1 4 7.201V8.8a.25.25 0 0 1-.382.212Zm10.043-.798-1.278.799A.25.25 0 0 1 12 8.799V7.2a.25.25 0 0 1 .383-.212l1.278.799a.25.25 0 0 1 0 .424Z"
+                />
+              </svg>
               {{ gameName(tournament.game_id) }}
             </h4>
             <h5 class="card-title">{{ tournament.name }}</h5>
