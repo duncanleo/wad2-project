99735a5bfeeadefa3ebf384614d150ec09786dfb
Brant Neo <brantneozc@gmail.com>
2021-11-13 03:39:49 +0800

    Styled Tournaments
    Styled the create tournament button
    Added icons to tournaments' info

--- src/frontend/screens/Tournaments/index.vue
+++ src/frontend/screens/Tournaments/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1 class="text-white">
+  <div class="row">
+    <h1 class="text-white col">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="36"
@@ -14,6 +14,17 @@
         /></svg
       >Tournaments
     </h1>
+    <div class="col">
+      <router-link
+        to="/tournaments/new"
+        v-if="isOrganiser"
+        class="btn border border-1 text-white"
+        style="text-decoration: none; background-color: #729b98; float: right"
+      >
+        New Tournament
+      </router-link>
+    </div>
+
     <div class="dropdown mb-5">
       <button
         class="btn btn-secondary dropdown-toggle"
@@ -53,7 +64,7 @@
         style="width: 100%"
       >
         <li
-          style="height: 50px; font-size: 25px; background-image: url()"
+          style="height: 50px; font-size: 25px"
           v-for="game in games"
           v-bind:value="game.name"
           v-bind:key="game.id"
@@ -63,15 +74,12 @@
         </li>
       </ul>
     </div>
-    <router-link to="/tournaments/new" v-if="isOrganiser">
-      New Tournament
-    </router-link>
 
     <br />
 
     <div
       style="background-color: #729b98"
-      class="p-4 row mt-4"
+      class="p-4 row mt-4 mb-5"
       v-if="selectedGame !== 'Select a game'"
     >
       <h1 class="text-white">
@@ -114,14 +122,95 @@
               </svg>
               {{ tournament.name }}
             </h4>
-            <p class="card-text">{{ tournament.start_at }}</p>
-            <p class="card-text">{{ tournament.end_at }}</p>
-            <p class="card-text">USD {{ tournament.prize_pool }}</p>
-            <p class="card-text">{{ tournament.owner.display_name }}</p>
+            <p class="card-text">
+              <span style="font-weight: bold; font-size: 18px; color: #729b98"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-clock-fill mb-1 me-1"
+                  style="color: #729b98"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                  /></svg
+                >From: </span
+              >{{ tournament.start_at }}
+            </p>
+            <p class="card-text">
+              <span style="font-weight: bold; font-size: 18px; color: #729b98"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-clock-fill mb-1 me-1"
+                  style="color: #729b98"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                  /></svg
+                >To: </span
+              >{{ tournament.end_at }}
+            </p>
+            <p class="card-text">
+              <span style="font-weight: bold; font-size: 18px; color: #729b98">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-cash-stack mb-1 me-1"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
+                  />
+                  <path
+                    d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z"
+                  /></svg
+                >Pool: USD
+              </span>
+              {{ tournament.prize_pool }}
+            </p>
+            <p class="card-text">
+              <span style="font-weight: bold; font-size: 18px; color: #729b98"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-fill mb-1 me-1"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+                  /></svg
+                >Organizer: </span
+              >{{ tournament.owner.display_name }}
+            </p>
             <a
               :href="tournament.url"
               class="btn border border-1 text-white"
               style="background-color: #729b98"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-link-45deg mb-1 me-1"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"
+                />
+                <path
+                  d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"
+                /></svg
               >More Details</a
             >
           </div>
@@ -179,14 +268,6 @@ const Tournaments = Vue.extend({
       }
       return tournamentData;
     },
-    isOrganiser() {
-      const state = this.$store.state as App.Frontend.Store.RootState;
-
-      return state.user?.type === 'organiser';
-    },
-  },
-
-  computed: {
     isOrganiser() {
       const state = this.$store.state as App.Frontend.Store.RootState;
 
