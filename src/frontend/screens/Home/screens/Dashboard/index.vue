8b28ea234333aa3a35c917973da4cc4738766bc9
Duncan Leo <hello@duncanleo.me>
2021-11-13 13:32:58 +0800

    refactor: make reusable Tournament component

--- src/frontend/screens/Home/screens/Dashboard/index.vue
+++ src/frontend/screens/Home/screens/Dashboard/index.vue
@@ -70,33 +70,11 @@
     </div>
     <div class="row" v-if="tournaments.length > 0">
       <h1 class="text-white">Tournaments</h1>
-      <div
-        v-for="tournament in tournamentData"
+      <tournament
         v-bind:key="tournament.id"
-        style="text-decoration: none"
-        class="fs-5 text-white col-3 mb-4"
-      >
-        <div
-          style="background-color: #729b98; height: 100px"
-          class="text-center my-auto rounded pt-3"
-          @click="redirect(tournament.url)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            class="bi bi-dpad-fill mb-2"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M6.5 0A1.5 1.5 0 0 0 5 1.5v3a.5.5 0 0 1-.5.5h-3A1.5 1.5 0 0 0 0 6.5v3A1.5 1.5 0 0 0 1.5 11h3a.5.5 0 0 1 .5.5v3A1.5 1.5 0 0 0 6.5 16h3a1.5 1.5 0 0 0 1.5-1.5v-3a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 0 16 9.5v-3A1.5 1.5 0 0 0 14.5 5h-3a.5.5 0 0 1-.5-.5v-3A1.5 1.5 0 0 0 9.5 0h-3Zm1.288 2.34-.799 1.278A.25.25 0 0 0 7.201 4H8.8a.25.25 0 0 0 .212-.382l-.799-1.279a.25.25 0 0 0-.424 0Zm0 11.32-.799-1.277A.25.25 0 0 1 7.201 12H8.8a.25.25 0 0 1 .212.383l-.799 1.278a.25.25 0 0 1-.424 0Zm-4.17-4.65-1.279-.798a.25.25 0 0 1 0-.424l1.279-.799A.25.25 0 0 1 4 7.201V8.8a.25.25 0 0 1-.382.212Zm10.043-.798-1.278.799A.25.25 0 0 1 12 8.799V7.2a.25.25 0 0 1 .383-.212l1.278.799a.25.25 0 0 1 0 .424Z"
-            />
-          </svg>
-          <span>{{ tournament.name }} </span>
-        </div>
-      </div>
+        v-for="tournament in tournaments"
+        v-bind:tournament="tournament"
+      />
     </div>
     <div
       class="row mb-5"
@@ -267,6 +245,7 @@
 import axios from 'axios';
 import { debounce } from 'lodash';
 import Vue from 'vue';
+import Tournament from '../../../../components/Tournament.vue';
 
 interface SearchResponse extends App.API.ResponseBase {
   players: App.API.User[];
@@ -276,6 +255,12 @@ interface SearchResponse extends App.API.ResponseBase {
 }
 
 const Dashboard = Vue.extend({
+  name: 'Dashboard',
+
+  components: {
+    Tournament,
+  },
+
   data() {
     return {
       searchTerm: '',
