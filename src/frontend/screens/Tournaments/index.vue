c8a0e17c7b40b9ca5b90158c49d545094b7349c4
Duncan Leo <hello@duncanleo.me>
2021-11-13 13:32:58 +0800

    refactor: make reusable Tournament component

--- src/frontend/screens/Tournaments/index.vue
+++ src/frontend/screens/Tournaments/index.vue
@@ -101,123 +101,11 @@
         <hr />
 
         <div class="row justify-content-center">
-          <div
-            class="col-4 card m-4 text-white"
-            style="width: 18rem; background-color: #31625f"
-            v-for="tournament of tournamentDetails"
-          >
-            <div class="card-body">
-              <h4 class="card-title">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  class="bi bi-dpad-fill mb-2 me-1"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6.5 0A1.5 1.5 0 0 0 5 1.5v3a.5.5 0 0 1-.5.5h-3A1.5 1.5 0 0 0 0 6.5v3A1.5 1.5 0 0 0 1.5 11h3a.5.5 0 0 1 .5.5v3A1.5 1.5 0 0 0 6.5 16h3a1.5 1.5 0 0 0 1.5-1.5v-3a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 0 16 9.5v-3A1.5 1.5 0 0 0 14.5 5h-3a.5.5 0 0 1-.5-.5v-3A1.5 1.5 0 0 0 9.5 0h-3Zm1.288 2.34-.799 1.278A.25.25 0 0 0 7.201 4H8.8a.25.25 0 0 0 .212-.382l-.799-1.279a.25.25 0 0 0-.424 0Zm0 11.32-.799-1.277A.25.25 0 0 1 7.201 12H8.8a.25.25 0 0 1 .212.383l-.799 1.278a.25.25 0 0 1-.424 0Zm-4.17-4.65-1.279-.798a.25.25 0 0 1 0-.424l1.279-.799A.25.25 0 0 1 4 7.201V8.8a.25.25 0 0 1-.382.212Zm10.043-.798-1.278.799A.25.25 0 0 1 12 8.799V7.2a.25.25 0 0 1 .383-.212l1.278.799a.25.25 0 0 1 0 .424Z"
-                  />
-                </svg>
-                {{ tournament.name }}
-              </h4>
-              <p class="card-text">
-                <span style="font-weight: bold; font-size: 18px; color: #729b98"
-                  ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-clock-fill mb-1 me-1"
-                    style="color: #729b98"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-                    /></svg
-                  >From: </span
-                >{{ tournament.start_at }}
-              </p>
-              <p class="card-text">
-                <span style="font-weight: bold; font-size: 18px; color: #729b98"
-                  ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-clock-fill mb-1 me-1"
-                    style="color: #729b98"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-                    /></svg
-                  >To: </span
-                >{{ tournament.end_at }}
-              </p>
-              <p class="card-text">
-                <span
-                  style="font-weight: bold; font-size: 18px; color: #729b98"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-cash-stack mb-1 me-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-                    />
-                    <path
-                      d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z"
-                    /></svg
-                  >Pool: USD
-                </span>
-                {{ tournament.prize_pool }}
-              </p>
-              <p class="card-text">
-                <span style="font-weight: bold; font-size: 18px; color: #729b98"
-                  ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-person-fill mb-1 me-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-                    /></svg
-                  >Organizer: </span
-                >{{ tournament.owner.display_name }}
-              </p>
-              <a
-                :href="tournament.url"
-                class="btn border border-1 text-white"
-                style="background-color: #729b98"
-                target="_blank"
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-link-45deg mb-1 me-1"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"
-                  />
-                  <path
-                    d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"
-                  /></svg
-                >More Details</a
-              >
-            </div>
-          </div>
+          <tournament
+            v-bind:key="tournament.id"
+            v-for="tournament in tournaments"
+            v-bind:tournament="tournament"
+          />
 
           <div v-if="results().length < 1">
             <h2 class="text-white">No upcoming tournaments available</h2>
@@ -231,6 +119,7 @@
 <script lang="ts">
 import axios from 'axios';
 import Vue from 'vue';
+import Tournament from '../../components/Tournament.vue';
 
 interface GamesResponse extends App.API.ResponseBase {
   games: App.API.Game[];
@@ -241,6 +130,10 @@ interface TournamentsResponse extends App.API.ResponseBase {
 }
 
 const Tournaments = Vue.extend({
+  components: {
+    Tournament,
+  },
+
   data() {
     return {
       games: [] as App.API.Game[],
