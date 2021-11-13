33561b8553951cd41c473144080ab3c5c7fd40cf
Duncan Leo <hello@duncanleo.me>
2021-11-14 00:13:21 +0800

    chore(Dashboard): use Player and Team components in search results

--- src/frontend/screens/Home/screens/Dashboard/index.vue
+++ src/frontend/screens/Home/screens/Dashboard/index.vue
@@ -10,33 +10,16 @@
       placeholder="Search"
     />
 
-    <div class="row" v-if="players.length > 0">
+    <div class="row mt-4" v-if="players.length > 0">
       <h1 class="text-white">Players</h1>
       <router-link
         v-bind:to="playerLink(player.id)"
         v-for="player in players"
         v-bind:key="player.id"
         style="text-decoration: none"
-        class="fs-5 text-white col-2 mb-4 text-center"
+        class="fs-5 text-white mb-4 text-center col-lg-4 col-md-6 col-12"
       >
-        <br />
-        <span class="mx-auto d-block text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="35"
-            height="35"
-            fill="currentColor"
-            class="bi bi-person-circle mb-1"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fill-rule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-        </span>
-        {{ player.display_name }}
+        <player v-bind:player="player" />
       </router-link>
     </div>
     <div class="row" v-if="teams.length > 0">
@@ -46,25 +29,9 @@
         v-for="team in teams"
         v-bind:key="team.id"
         style="text-decoration: none"
-        class="fs-5 text-white col-2 mb-4 mx-2 text-center"
+        class="fs-5 text-white col-12 mb-4 mx-2 text-center"
       >
-        <br />
-        <span class="mx-auto d-block text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="currentColor"
-            class="bi bi-joystick mb-1 me-1"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2z"
-            />
-            <path
-              d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23z"
-            /></svg></span
-        >{{ team.name }}
+        <team v-bind:team="team" />
       </router-link>
     </div>
     <div class="row" v-if="tournaments.length > 0">
@@ -123,6 +90,8 @@ import axios from 'axios';
 import { debounce } from 'lodash';
 import Vue from 'vue';
 import Tournament from '../../../../components/Tournament.vue';
+import Player from '../../../../components/Player.vue';
+import Team from '../../../../components/Team.vue';
 
 interface SearchResponse extends App.API.ResponseBase {
   players: App.API.User[];
@@ -136,6 +105,8 @@ const Dashboard = Vue.extend({
 
   components: {
     Tournament,
+    Player,
+    Team,
   },
 
   data() {
