223ae48ef4c6f427db272f6116794192e0dd5ad8
Duncan Leo <hello@duncanleo.me>
2021-11-14 12:36:16 +0800

    feat: Site footer

--- src/frontend/VueApp.vue
+++ src/frontend/VueApp.vue
@@ -4,16 +4,19 @@
     <main class="container-fluid p-0">
       <router-view></router-view>
     </main>
+    <site-footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import SiteHeader from './components/SiteHeader.vue';
+import SiteFooter from './components/SiteFooter.vue';
 
 const App = Vue.extend({
   components: {
     SiteHeader,
+    SiteFooter,
   },
 });
 
