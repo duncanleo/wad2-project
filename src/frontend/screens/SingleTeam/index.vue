23b57cb48e01fa5e50704e4db80f825a45284934
FieryCake <glenn-ham@hotmail.com>
2021-11-14 21:29:30 +0800

    teams join tournament
    teams are now able to join tournaments from singleTeam screen

--- src/frontend/screens/SingleTeam/index.vue
+++ src/frontend/screens/SingleTeam/index.vue
@@ -127,13 +127,16 @@
     </div>
 
     <div class="row mt-4">
-      <span><h4 class="text-white fw-bold">Tournaments </h4><button
-              class="btn btn-success text-white fw-bold me-2" data-bs-toggle="modal"
+      <span
+        ><h4 class="text-white fw-bold">Tournaments</h4>
+        <button
+          class="btn btn-success text-white fw-bold me-2"
+          data-bs-toggle="modal"
           data-bs-target="#participateTourney"
-            >
-              Participate
-            </button></span>
-      
+        >
+          Participate
+        </button></span
+      >
 
       <div
         class="col-lg-4 col-md-6 col-12 mb-3"
@@ -149,80 +152,135 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Participate in a Tournament</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
           <div class="modal-body">
-
-            <p>{{selectedGameId}}</p>
-            {{results}}
-            
             <div class="dropdown w-100">
-          <button
-            class="
-              btn btn-secondary
-              dropdown-toggle
-              w-100
-              d-flex
-              align-items-center
-            "
-            type="button"
-            id="game-dropdown-button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              class="bi bi-controller me-2"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1v-1z"
-              />
-              <path
-                d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729c.14.09.266.19.373.297.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466.206.875.34 1.78.364 2.606.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527-1.627 0-2.496.723-3.224 1.527-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.34 2.34 0 0 1 .433-.335.504.504 0 0 1-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a13.748 13.748 0 0 0-.748 2.295 12.351 12.351 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.354 12.354 0 0 0-.339-2.406 13.753 13.753 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27-1.036 0-2.063.091-2.913.27z"
-              />
-            </svg>
-            {{ (selectedGame != null && selectedGame.name) || 'Select a game' }}
-          </button>
-
-          <ul
-            class="dropdown-menu"
-            aria-labelledby="game-dropdown-button"
-            style="width: 100%"
-          >
-            <li>
-              <a
-                href="#"
-                class="dropdown-item text-tertiary"
-                v-on:click="setSelectedGameId(null)"
-                >Select a game</a
+              <button
+                class="
+                  btn btn-secondary
+                  dropdown-toggle
+                  w-100
+                  d-flex
+                  align-items-center
+                "
+                type="button"
+                id="game-dropdown-button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
               >
-            </li>
-            <li
-              v-for="game in games"
-              v-bind:value="game.name"
-              v-bind:key="game.id"
-              v-on:click="setSelectedGameId(game.id)"
-            >
-              <a href="#" class="dropdown-item">{{ game.name }}</a>
-            </li>
-          </ul>
-        </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  class="bi bi-controller me-2"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M11.5 6.027a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm-1.5 1.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm-6.5-3h1v1h1v1h-1v1h-1v-1h-1v-1h1v-1z"
+                  />
+                  <path
+                    d="M3.051 3.26a.5.5 0 0 1 .354-.613l1.932-.518a.5.5 0 0 1 .62.39c.655-.079 1.35-.117 2.043-.117.72 0 1.443.041 2.12.126a.5.5 0 0 1 .622-.399l1.932.518a.5.5 0 0 1 .306.729c.14.09.266.19.373.297.408.408.78 1.05 1.095 1.772.32.733.599 1.591.805 2.466.206.875.34 1.78.364 2.606.024.816-.059 1.602-.328 2.21a1.42 1.42 0 0 1-1.445.83c-.636-.067-1.115-.394-1.513-.773-.245-.232-.496-.526-.739-.808-.126-.148-.25-.292-.368-.423-.728-.804-1.597-1.527-3.224-1.527-1.627 0-2.496.723-3.224 1.527-.119.131-.242.275-.368.423-.243.282-.494.575-.739.808-.398.38-.877.706-1.513.773a1.42 1.42 0 0 1-1.445-.83c-.27-.608-.352-1.395-.329-2.21.024-.826.16-1.73.365-2.606.206-.875.486-1.733.805-2.466.315-.722.687-1.364 1.094-1.772a2.34 2.34 0 0 1 .433-.335.504.504 0 0 1-.028-.079zm2.036.412c-.877.185-1.469.443-1.733.708-.276.276-.587.783-.885 1.465a13.748 13.748 0 0 0-.748 2.295 12.351 12.351 0 0 0-.339 2.406c-.022.755.062 1.368.243 1.776a.42.42 0 0 0 .426.24c.327-.034.61-.199.929-.502.212-.202.4-.423.615-.674.133-.156.276-.323.44-.504C4.861 9.969 5.978 9.027 8 9.027s3.139.942 3.965 1.855c.164.181.307.348.44.504.214.251.403.472.615.674.318.303.601.468.929.503a.42.42 0 0 0 .426-.241c.18-.408.265-1.02.243-1.776a12.354 12.354 0 0 0-.339-2.406 13.753 13.753 0 0 0-.748-2.295c-.298-.682-.61-1.19-.885-1.465-.264-.265-.856-.523-1.733-.708-.85-.179-1.877-.27-2.913-.27-1.036 0-2.063.091-2.913.27z"
+                  />
+                </svg>
+                {{
+                  (selectedGame != null && selectedGame.name) || 'Select a game'
+                }}
+              </button>
 
+              <ul
+                class="dropdown-menu"
+                aria-labelledby="game-dropdown-button"
+                style="width: 100%"
+              >
+                <li>
+                  <a
+                    href="#"
+                    class="dropdown-item text-tertiary"
+                    v-on:click="setSelectedGameId(null)"
+                    >Select a game</a
+                  >
+                </li>
+                <li
+                  v-for="game in games"
+                  v-bind:value="game.name"
+                  v-bind:key="game.id"
+                  v-on:click="setSelectedGameId(game.id)"
+                >
+                  <a href="#" class="dropdown-item">{{ game.name }}</a>
+                </li>
+              </ul>
+            </div>
+
+            <div
+              class="col-12 mt-4 mb-5"
+              v-if="selectedGame !== 'Select a game'"
+            >
+              <h4 class="text-tertiary fw-bold mb-3">Upcoming Tournaments</h4>
+
+              <span
+                class="text-tertiary"
+                v-if="upcomingTournaments.length === 0"
+              >
+                There are no upcomingTournaments
+              </span>
+
+              <div class="row justify-content-center" style="row-gap: 1rem">
+                <ul class="list-group">
+                  <li
+                    class="list-group-item"
+                    v-for="tournament of upcomingTournaments"
+                    v-bind:key="tournament.id"
+                  >
+                    <div class="row align-items-center bg-dark text-white">
+                      <div class="col-4">
+                        <img
+                          class="w-100"
+                          v-bind:src="tournament.game.banner_image"
+                        />
+                      </div>
+                      <div class="col-4 text-center">
+                        <span>{{ tournament.name }}</span>
+                      </div>
+                      <div class="col-2 text-center">
+                        ${{ tournament.prize_pool }}}
+                      </div>
+                      <div class="col-2">
+                        <button
+                          class="btn btn-danger text-white fw-bold"
+                          v-on:click="joinTournament(tournament.id)"
+                        >
+                          Join
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+
+                <div v-if="results.length < 1">
+                  <h2 class="text-white">No tournament available</h2>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
           </div>
         </div>
       </div>
     </div>
-
-
-
   </div>
 </template>
 
@@ -252,7 +310,6 @@ const SingleTeam = Vue.extend({
   components: {
     Player,
   },
-  
 
   data() {
     return {
@@ -305,9 +362,34 @@ const SingleTeam = Vue.extend({
         return start.isAfter(now);
       });
     },
+    pastTournaments() {
+      const now = moment();
+
+      return this.results.filter((tournament) => {
+        const start = moment(tournament.start_at);
+
+        return start.isSameOrBefore(now);
+      });
+    },
   },
 
   methods: {
+    joinTournament(tourneyId) {
+      console.log(tourneyId);
+      console.log(this.team);
+
+      const response = axios
+        .put(`/api/tournaments/${tourneyId}/participate`, {
+          team_id: this.team.id,
+        })
+
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
     async fetchTournaments() {
       const response = await axios.get<TournamentsResponse>('/api/tournaments');
 
@@ -336,7 +418,6 @@ const SingleTeam = Vue.extend({
           console.log(error.message);
         });
     },
-    
 
     async fetchTeamData() {
       const teamID = this.$route.params.id;
